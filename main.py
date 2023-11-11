4421d443f19c5329fb031fad658ce0476db00fad
4EloBek144 <150430914+4EloBek144@users.noreply.github.com>
2023-11-11 21:41:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,32 @@ import random
 import sys
 from random import randrange
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QInputDialog
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QInputDialog, QMainWindow
 from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class RandomO(QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(500, 382)
+        self.pb = QtWidgets.QPushButton(Form)
+        self.pb.setGeometry(QtCore.QRect(130, 110, 221, 121))
+        self.pb.setObjectName("pb")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pb.setText(_translate("Form", "Кнопка???"))
+
+
+class MyWidget(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.initUI()
 
     def initUI(self):
@@ -30,16 +48,21 @@ class RandomO(QWidget):
 
     def draw(self, qp):
         self.ra = random.randrange(30, 500)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randrange(0, 256), \
+                           random.randrange(0, 256), random.randrange(0, 256)))
         qp.drawEllipse(30, 30, self.ra, self.ra)
         self.ra = random.randrange(30, 500)
+        qp.setBrush(QColor(random.randrange(0, 256), \
+                           random.randrange(0, 256), random.randrange(0, 256)))
         qp.drawEllipse(200, 300, self.ra, self.ra)
         self.ra = random.randrange(30, 500)
+        qp.setBrush(QColor(random.randrange(0, 256), \
+                           random.randrange(0, 256), random.randrange(0, 256)))
         qp.drawEllipse(100, 50, self.ra, self.ra)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = RandomO()
+    ex = MyWidget()
     ex.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
